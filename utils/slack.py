550375597b971e47bf14536e92bf1d6222bdadec
Minh Trinh <141774973+quangminhtrinh@users.noreply.github.com>
2026-04-13 14:36:55 -0400

    Refine comment on MEMBERS_CACHE_TTL
    Clarified comment on MEMBERS_CACHE_TTL for better understanding.

--- utils/slack.py
+++ utils/slack.py
@@ -51,7 +51,7 @@ def resolve_owner_mentions_to_emails(owners: list[str]) -> list[str]:
 
 _members_cache = None
 _members_cache_time = 0
-MEMBERS_CACHE_TTL = 300  # 5 minutes -> would be stale if members are added/removed but reduces API calls for large workspaces
+MEMBERS_CACHE_TTL = 300  # 5 minutes; would be stale if members are added/removed but reduces API calls for large workspaces
 
 def search_workspace_members(query: str) -> list:
     """
